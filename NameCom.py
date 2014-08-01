bd15c24d8a073f8947e9165ad8c19ec7c3d33cec
Dong Zhu <bluezhudong@gmail.com>
2014-08-01 09:28:06 +0800

    NameCom: Refine and Minimize code redundancy
    Add class authentication method
    Add usage function
    
    Signed-off-by: Dong Zhu <bluezhudong@gmail.com>

--- NameCom.py
+++ NameCom.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
 import json
-import subprocess, time, re
+import sys, subprocess, time, re
 import urllib
 import urllib2
 
-class NameComDomain:
+class NameComDomain(object):
     def __init__(self):
         """docstring for __init__"""
         self.BASE_URL = "https://api.name.com/api/"
@@ -26,74 +26,66 @@ class NameComDomain:
             self.session_token = obj["session_token"]
             return obj["session_token"]
 
-    def get_account(self):
-        """get name.com account info"""
-        #print "Trying to get account information ..."
-        PATH = self.BASE_URL + "account/get"
+    def authentication(self, url, data=None):
+        """authentication method"""
+        PATH = self.BASE_URL + url
         req = urllib2.Request(PATH)
         req.add_header('Api-Session-Token', self.session_token)
 
-        res = urllib2.urlopen(req).read()
+        res = urllib2.urlopen(req, data).read()
         obj = json.loads(res)
         if obj["result"]["code"] == 100:
+            return obj
+        else:
+            return False
+
+    def get_account(self):
+        """get name.com account info"""
+        #print "Trying to get account information ..."
+        res = self.authentication('account/get')
+        if res:
             print "Account information get successfully!"
 
     def list_domain(self):
         """list the current available domains"""
-        PATH = self.BASE_URL + "domain/list"
-        req = urllib2.Request(PATH)
-        req.add_header('Api-Session-Token', self.session_token)
-
-        res = urllib2.urlopen(req).read()
-        #print res
+        print "Listing the existing domains:"
+        res = self.authentication('domain/list')
+        if res:
+            for i in res["domains"].keys():
+                print "->  ""www." + i
 
     def dns_records(self):
         """list the current available domains"""
-        PATH = self.BASE_URL + "dns/list/bluezd.info"
-        req = urllib2.Request(PATH)
-        req.add_header('Api-Session-Token', self.session_token)
-
-        res = urllib2.urlopen(req).read()
-        obj = json.loads(res)
-        records = obj["records"]
-
-        return records
+        res = self.authentication('dns/list/bluezd.info')
+        if res:
+            records = res["records"]
+            return records
 
     def del_dnsrecords(self, record_id):
         """delete the existing dns record"""
-        PATH = self.BASE_URL + "dns/delete/bluezd.info"
-        req = urllib2.Request(PATH)
-        req.add_header('Api-Session-Token', self.session_token)
-
         records = dict()
         records["record_id"] = record_id
-
         data = json.dumps(records)
-        response = urllib2.urlopen(req, data).read()
-        obj = json.loads(response)
-        if obj["result"]["code"] == 100:
+
+        res = self.authentication('dns/delete/bluezd.info', data)
+        if res:
             print "Delete record Successfully!"
 
     def add_dnsrecords(self, domain_name, ip_addr):
         """add a new dns record"""
-        PATH = self.BASE_URL + "dns/create/bluezd.info"
-        req = urllib2.Request(PATH)
-        req.add_header('Api-Session-Token', self.session_token)
-
         records = dict()
         records["hostname"] = domain_name
         records["type"] = "A"
         records["content"] = ip_addr
         records["ttl"] = 300
         records["priority"] = "N/A"
-
         data = json.dumps(records)
-        response = urllib2.urlopen(req, data).read()
-        obj = json.loads(response)
-        if obj["result"]["code"] == 100:
+
+        res = self.authentication('dns/create/bluezd.info', data)
+        if res:
             print "Add record Successfully!"
         else:
-            print response
+            print res
     
 def ddns_namecom(ip_addr):
     """main funciton"""
@@ -118,9 +110,17 @@ def ddns_namecom(ip_addr):
                 namecom.del_dnsrecords(records[i]["record_id"])
 
             i += 1
-
         # add new dns record
         namecom.add_dnsrecords("home-ddns", ip_addr)
 
+def usage():
+    """usage of NameCom.py"""
+    print "python NameCom.py <IP address>"
+    sys.exit(1)
+
 if __name__ == '__main__':
-    ddns_namecom("A.B.C.D")
+    if len(sys.argv) > 1:
+        ddns_namecom(sys.argv[1])
+        sys.exit(0)
+    else:
+        usage()
